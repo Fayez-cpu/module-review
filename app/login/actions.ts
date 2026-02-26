93dfbcff23196567f8450ce8fb25cfa321a318ee
Fayez-cpu <100725949@unimail.derby.ac.uk>
2026-02-26 17:34:29 +0000

    n

--- app/login/actions.ts
+++ app/login/actions.ts
@@ -3,19 +3,13 @@
 import { signIn } from "@/auth"
 
 export async function requestMagicLink(formData: FormData) {
-  const studentId = String(formData.get("email") ?? "").trim()
-  console.log(formData)
-  // Validate it's a number
-  if (!/^\d+$/.test(studentId)) {
-    throw new Error("Student ID must be numeric")
-  }
+
 
   // Construct university email
-  const email = `${studentId}@unimail.derby.ac.uk`
-  formData.set("email", email)
+
 
     await signIn("resend", {
-    email,
+    formData,
     redirectTo: "/onboarding",
   })
 }
